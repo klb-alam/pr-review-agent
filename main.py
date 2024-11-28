66ab4f8ba559fee1b815b0ed3721f784db9c2536
klb-alam <168710602+klb-alam@users.noreply.github.com>
2024-11-28 16:16:11 +0800

    configured

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def find_or_create_bot_comment(
 
 
 async def main():
-    load_dotenv(override=False)
+    load_dotenv(override=True)
     github_token = os.getenv("INPUT_GITHUB_TOKEN") or os.getenv("GITHUB_TOKEN")
 
     if not github_token:
@@ -93,10 +93,6 @@ async def main():
         pr_number = int(pr_number_str)
     except ValueError:
         raise ValueError("Invalid PR_NUMBER format.")
-    print("INPUT_OPENAI_API_KEY:", os.getenv("INPUT_OPENAI_API_KEY"))
-    print("INPUT_GITHUB_TOKEN:", os.getenv("INPUT_GITHUB_TOKEN"))
-    print("INPUT_PR_NUMBER:", os.getenv("INPUT_PR_NUMBER"))
-    print("GITHUB_ACTOR:", os.getenv("GITHUB_ACTOR"))
 
     gh = Github(github_token)
     repo = gh.get_repo("klb-alam/code-challenge")
